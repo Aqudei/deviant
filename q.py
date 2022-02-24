9cd37a1c3378a4e221099426ceaa67d29251b663
Aqudei <fdeu.aalmine@gmail.com>
2022-02-24 10:58:37 +0800

    mm

--- q.py
+++ q.py
@@ -1,6 +1,23 @@
 import deviant
+from requests_oauthlib import OAuth2Session
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+CLIENT_ID = os.environ.get('CLIENT_ID')
+REDIRECT_URL = os.environ.get('DA_REDIRECT_URL')
+authorization_base_url = 'https://www.deviantart.com/oauth2/authorize'
+
+# REDIRECT_URL = 'http://143.198.179.20/oauthlogin'
 
 if __name__ == "__main__":
-    client = deviant.DeviantArt()
-    client.list_watchers()
+    # client = deviant.DeviantArt()
+    # client.list_watchers()
+    
+    scope = 'comment.post'
+    deviant = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URL)
+    authorization_url, state = deviant.authorization_url(
+        authorization_base_url)
+
+    
+    print(authorization_url)
